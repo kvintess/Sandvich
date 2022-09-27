87118cc4571dbc98abc896a4b460a64433703792
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 17:33:28 +0300

    code is ready

--- Sandvich.py
+++ Sandvich.py
@@ -1,8 +1,20 @@
 class Bread:
-    def __init__(self):
+    def __str__(self):
         return 'Я хлеб'
 
     def __add__(self, other):
         return Sandvich(part1=self, part2=other)
 
-class Sausage
+class Sausage:
+
+    def __str__(self):
+        return 'Я колбаса'
+
+    def __add__(self, other):
+        return Sandvich(part1=self, part2=other)
+
+class Sandvich:
+
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
